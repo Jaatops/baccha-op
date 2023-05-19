d3971361373c95be8a4c8a695a4d2926637fb3a8
SHOKINx2 <134042364+SHOKINx2@users.noreply.github.com>
2023-05-19 13:13:22 +0530

    Add files via upload

--- Indorix/__init__.py
+++ Indorix/__init__.py
@@ -1,1 +1,37 @@
+from Indorix.core.bot import IndorixBot
+from Indorix.core.dir import dirr
+from Indorix.core.git import git
+from Indorix.core.userbot import Userbot
+from Indorix.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
 
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = IndorixBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
